e93180eea7718eacbfb2bd2ace7b239863913c9d
Matt Croydon <mcroydon@gmail.com>
2014-01-26 06:51:19 -0600

    Locking is an implementation detail, don't expose locking.

--- similarity/similarity.go
+++ similarity/similarity.go
@@ -12,29 +12,29 @@ type Item struct {
 
 // Similarity is a similarity storage and retrieval engine.
 type Similarity struct {
-	sync.RWMutex
-	data map[string][]Item
+	mutex sync.RWMutex
+	data  map[string][]Item
 }
 
 // Create a new Similarity engine.
 func NewSimilarity() *Similarity {
-	return &Similarity{make(map[string][]Item)}
+	return &Similarity{data: make(map[string][]Item)}
 }
 
 // Add an Item to the engine with a key.
 func (sim *Similarity) Add(key string, item Item) {
-	sim.Lock()
+	sim.mutex.Lock()
 	sim.data[key] = append(sim.data[key], item)
-	sim.Unlock()
+	sim.mutex.Unlock()
 }
 
 // Get all the keys in this Similarity.
 func (sim *Similarity) Keys() []string {
-	sim.RLock()
+	sim.mutex.RLock()
 	keys := make([]string, 0, len(sim.data))
 	for k, _ := range sim.data {
 		keys = append(keys, k)
 	}
-	sim.RUnlock()
+	sim.mutex.RUnlock()
 	return keys
 }
