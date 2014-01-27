be7e332804a9b0260b2cd7b490753621472281e1
Matt Croydon <mcroydon@gmail.com>
2014-01-26 22:35:04 -0600

    Added ReadJson and WriteJson with tests.

--- similarity/similarity.go
+++ similarity/similarity.go
@@ -1,6 +1,9 @@
 package similarity
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"sync"
 )
 
@@ -37,4 +40,29 @@ func (sim *Similarity) Keys() []string {
 	}
 	sim.mutex.RUnlock()
 	return keys
+}
+
+// Dump the data backing a Similarity engine to a Writer.
+func (sim *Similarity) WriteJson(w io.Writer) error {
+	b, err := json.Marshal(sim.data)
+	if err != nil {
+		return err
+	}
+	w.Write(b)
+	return nil
+}
+
+// Read the data for a Similarity engine from a Writer and load it
+func (sim *Similarity) ReadJson(r io.Reader) error {
+	// TODO: Is it idomatic to return underlying errors such as those encountered by ReadFrom or Unmarshal?
+	b := new(bytes.Buffer)
+	_, err := b.ReadFrom(r)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(b.Bytes(), &sim.data)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- similarity/similarity_test.go
+++ similarity/similarity_test.go
@@ -1,8 +1,16 @@
 package similarity
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestLoad(t *testing.T) {
+var (
+	jsondata = []byte(`{"critic1":[{"Name":"In a World","Value":3.5},{"Name":"War Games","Value":4.5}],
+	"critic2":[{"Name":"In a World","Value":2},{"Name":"War Games","Value":3}]}`)
+)
+
+func populateSim() *Similarity {
 	critic1 := "critic1"
 	critic2 := "critic2"
 	rating1 := Item{"In a World", 3.5}
@@ -15,10 +23,44 @@ func TestLoad(t *testing.T) {
 	sim.Add(critic2, rating2)
 	sim.Add(critic1, rating3)
 	sim.Add(critic2, rating4)
+	return sim
+}
 
+func TestCreate(t *testing.T) {
+
+	sim := populateSim()
 	keys := sim.Keys()
-	t.Log(keys)
 	if len(keys) != 2 {
 		t.Errorf("Expected %v keys, had %v.", 2, len(keys))
+	}
+
+	buf := new(bytes.Buffer)
+
+	sim.WriteJson(buf)
+}
+
+func TestReadJson(t *testing.T) {
+	sim := NewSimilarity()
+	err := sim.ReadJson(bytes.NewBuffer(jsondata))
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	keys := sim.Keys()
+	expectedKeys := []string{"critic1", "critic2"}
+	if len(keys) == 2 && keys[0] != "critic1" || keys[1] != "critic2" {
+		t.Errorf("Expected keys %v found keys %v.", expectedKeys, keys)
+	}
+
+}
+
+func TestWriteJson(t *testing.T) {
+	sim := populateSim()
+	b := new(bytes.Buffer)
+	err := sim.WriteJson(b)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !bytes.Contains(b.Bytes(), []byte("In a World")) {
+		t.Errorf("Output does not contain expected value: %v", b.Bytes())
 	}
 }
