8bb4b1c6e6053bf875970c12eb59ddb3ed299a12
Matt Croydon <mcroydon@gmail.com>
2014-02-01 12:59:05 -0600

    Revert "Added examples to README."
    This reverts commit 85bc3cc6f6613578d9184c48a1dabcc8616901d8.

--- similarity/doc.go
+++ similarity/doc.go
@@ -1,34 +1,3 @@
 // Package similarity provides a simple similarity engine, similar to the recommendation engine created in
 // Chapter 2 of Programming Collective Intelligence.
-//
-// Example usage:
-//
-// // Create a similarity engine
-// sim := similarity.New()
-//
-// // Create some items
-// critic1 := "critic1"
-// critic2 := "critic2"
-// rating1 := similarity.Item{"In a World", 3.5}
-// rating2 := similarity.Item{"In a World", 2.0}
-// rating3 := similarity.Item{"War Games", 4.5}
-// rating4 := similarity.Item{"War Games", 3.0}
-//
-// // Add items to the similarity with a string key.
-// // In this case we are using the critic's name.
-// sim.Add(critic1, rating1)
-// sim.Add(critic1, rating3)
-// sim.Add(critic2, rating2)
-// sim.Add(critic2, rating4)
-//
-// // Get similar keys (in this case critics) and the
-// // rated items.
-// results := sim.SimilarEuclidean("critic1", 5)
-//
-// // We can then retrieve the critic's name and similarity score.
-// fmt.Println(results[0].Name)
-// fmt.Println(results[0].Similarity)
-// Output:
-// critic2
-// 0.32037724101704074
 package similarity
