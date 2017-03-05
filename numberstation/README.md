0f74a98e5086b36ba02ccfe6d2e9d2ffe56e1fbc
Matt Croydon <mcroydon@gmail.com>
2017-03-04 22:27:23 -0600

    Added numberstation.

---
+++ numberstation/README.md
@@ -0,0 +1,24 @@
+# numberstation
+
+I've been intrigued by [numbers stations](https://en.wikipedia.org/wiki/Numbers_station) for as long as I've
+known about them. I thought it would be fun to put together a small server that broadcast random numbers (in
+this case a random Uint64) over UDP Multicast.
+
+## Installation
+
+You can run numberstation from source:
+	cd $GOPATH/src
+	git clone git@github.com:mcroydon/goplayground.git
+	cd github.com/mcroydon/goplayground/numberstation
+	go run numberstation.go
+
+## Usage
+
+Numberstation takes two arguments but provides sane defaults:
+
+	-ip string
+		IP to send UDP traffic to. (default "224.0.0.1")
+	-port uint
+		Port number to use. (default 4815)
+	-seconds uint
+		Number of seconds betweeen broadcast. (default 1)
---
+++ numberstation/numberstation.go
@@ -0,0 +1,56 @@
+package main
+
+import "bytes"
+import "encoding/binary"
+import "flag"
+import "fmt"
+import "log"
+import "math/rand"
+import "net"
+import "time"
+
+
+func getAddr(ip string, port uint) string {
+	return fmt.Sprintf("%v:%v", ip, port)
+}
+
+func numbercast(ip string, port uint) {
+	number := rand.Uint64()
+	log.Printf("now serving: %v", number)
+
+	multicastAddr := getAddr(ip, port)
+
+	addr, err := net.ResolveUDPAddr("udp", multicastAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+	defer conn.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	err = binary.Write(buf, binary.BigEndian, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn.Write([]byte(buf.Bytes()))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	ip := flag.String("ip", "224.0.0.1", "IP to send UDP traffic to.")
+	port := flag.Uint("port", 4815, "Port number to use.")
+	seconds := flag.Uint("seconds", 1, "Number of seconds betweeen broadcast.")
+
+	flag.Parse()
+
+	log.Printf("Sending numbers to %v every %v seconds.", getAddr(*ip, *port), *seconds)
+
+	ticker := time.Tick(time.Duration(*seconds) * time.Second)
+
+	for _ = range ticker {
+		numbercast(*ip, *port)
+	}
+}
